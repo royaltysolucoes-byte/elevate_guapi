26eec113773c970feda46b3214b139da0f770cbe
royaltysolucoes <royaltysolucoes@gmail.com>
2025-11-03 02:32:52 -0300

    Fix missing confirmarSenha field in emails form reset

--- app/(protected)/dashboard/emails/page.tsx
+++ app/(protected)/dashboard/emails/page.tsx
@@ -162,6 +162,7 @@ export default function EmailsPage() {
               colaborador: '',
               nome: '',
               senha: '',
+              confirmarSenha: '',
             });
             setShowModal(true);
           }}
